4a2056e0b1b33509c1bdfcd37bad865dee6aee05
Seb <sebastien.philippe5@gmail.com>
2019-10-22 10:52:00 +0200

    iot_serverBox: Ajout de la route add evenement

---
+++ iot_letterBox/application.py
@@ -0,0 +1,11 @@
+import json, requests
+import os,time
+
+def createEvent():
+    print("évenement crée")
+
+
+while True:
+    createEvent()
+    time.sleep(5)
+
--- iot_serverBox/app.py
+++ iot_serverBox/app.py
@@ -16,7 +16,12 @@ from classes.evenements import EventEmitter
 
 
 @app.route('/')
-def index():
+def index5():
+    events= EventEmitter.query.limit(5)
+    return render_template('index.html',events=events)
+
+@app.route('/event')
+def indexAll():
     events= EventEmitter.query.all()
     return render_template('index.html',events=events)
 
@@ -24,6 +29,18 @@ def index():
 def deleteEvent(id):
     event = EventEmitter.query.filter_by(id=id).first_or_404()# si jamais l'id n'existe pas
     db.session.delete(event)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/event/add',methods=["POST"])
+def addEvent():
+    # crée un événement
+    idEmetteur = request.data['idEmetteur']
+    typeEvenement = request.data['typeEvenement']
+    # Création de l'événement
+    event = EventEmitter(idEmetteur=idEmetteur, typeEvenement=typeEvenement)
+    #enregistre en bdd
+    db.session.add(event)
     db.session.commit()
     return redirect(url_for('index'))
 
