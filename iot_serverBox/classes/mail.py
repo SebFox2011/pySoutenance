4f51193e6723e76ad1f11e74ca753030559010ad
Seb <sebastien.philippe5@gmail.com>
2019-10-25 13:56:58 +0200

    iot_serverBox: Correction de la partie mail et à tester sur un server autorisant le smtp

--- iot_serverBox/classes/mail.py
+++ iot_serverBox/classes/mail.py
@@ -3,17 +3,22 @@ import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def envoiMail():
+#context = ssl.create_default_context()
+
+def envoiMail(text):
     print("fonction")
-    my_smtp = smtplib.SMTP_SSL('smtp.gmail.com', 587)
-    print (my_smtp)
-    print("1")
-    my_smtp.login('test@gmail.com',getpass.getpass())
-    print("2")
-    my_smtp.sendmail('test@gmail.com','test@gmail.com',"text")
-    print("3")
-    my_smtp.quit()
-    print("4")
+    #my_smtp = smtplib.SMTP_SSL('smtp.gmail.com', 587)
+    try:
+        my_smtp = smtplib.SMTP_SSL('smtp.googlemail.com',465)#,context=context)
+        print("1")
+        my_smtp.login('test@gmail.com','PASSWORD')
+        print("2")
+        my_smtp.sendmail('test@gmail.com','test@gmail.com',text)
+        print("3")
+        my_smtp.quit()
+        print("4")
+    except:
+        print("error")
 
 def envoiMail2():
     msg = MIMEMultipart()
